7a6dbf1f55af15e27245c808f8237bdc4499309b
sabinbabu <sabinacharya444@gmail.com>
2025-03-09 17:03:11 +1100

    initial setup

---
+++ router/emailRouter.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const emailRouter = express.Router();
+
+// email router end point
+
+export default emailRouter;
---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import emailRouter from "./router/emailRouter";
+
+const app = express();
+const PORT = process.env.PROD || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/email", emailRouter);
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error.message)
+    : console.log("Server running successfully at http://localhost:" + PORT);
+});
