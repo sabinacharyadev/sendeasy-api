c1113a20d4b948b9faf4f23a2f477532569eb59a
sabinbabu <sabinacharya444@gmail.com>
2025-03-10 16:22:17 +1100

    gmail smtp added

--- router/emailRouter.js
+++ router/emailRouter.js
@@ -1,7 +1,23 @@
 import express from "express";
+import { sendEmailToClients } from "../utility/nodeMailerHelper.js";
+import {
+  buildErrorResponse,
+  buildSuccessResponse,
+} from "../utility/responseHelper.js";
 
 const emailRouter = express.Router();
 
-// email router end point
+emailRouter.post("/", async (req, res) => {
+  try {
+    const { emails } = req.body;
+    const success = await sendEmailToClients(emails);
+    success
+      ? buildSuccessResponse(res, success, "Emails sent successfully")
+      : buildErrorResponse(res, "Something went wrong");
+  } catch (error) {
+    console.log(error);
+    buildErrorResponse(res, "Something went wrong");
+  }
+});
 
 export default emailRouter;
